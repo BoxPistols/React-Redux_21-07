5075f97df030e2215d3f57531a44d6ebd930d8d6
A_ito <BoxPistols@users.noreply.github.com>
2021-07-02 19:21:54 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./styles.css";
+
+import { createStore } from "redux";
+// reducer function (init, action) switch...
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "PLUS_ONE":
+      return state + 1;
+    case "MINUS_ONE":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+// console.log(store);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({ type: "PLUS_ONE" });
+store.dispatch({ type: "MINUS_ONE" });
+store.dispatch({ type: "MINUS_ONE" });
+store.dispatch({ type: "MINUS_ONE" });
+
+// ==============
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Edit to see some magic happen!</h2>
+    </div>
+  );
+}
