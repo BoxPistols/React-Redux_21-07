27aee479ce28cfb733805512015f8d3f85c56252
A_ito <BoxPistols@users.noreply.github.com>
2021-07-02 19:24:57 +0900

    init on s-box

--- src/App.js
+++ src/App.js
@@ -12,15 +12,16 @@ const reducer = (state = 0, action) => {
       return state;
   }
 };
-
+// store create use reducer
 const store = createStore(reducer);
-
 // console.log(store);
 
+// move getState
 store.subscribe(() => {
   console.log(store.getState());
 });
 
+// open dispatch use type
 store.dispatch({ type: "PLUS_ONE" });
 store.dispatch({ type: "MINUS_ONE" });
 store.dispatch({ type: "MINUS_ONE" });
