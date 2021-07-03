b8e33c2059dc7e3b9a05842128ee0a3cc6d0f198
ait <ait@ex.in>
2021-07-03 19:10:41 +0900

    setting / Prettier on Air

--- src/App.js
+++ src/App.js
@@ -2,42 +2,42 @@ import './styles.scss'
 import { connect } from 'react-redux'
 
 const App = ({ number, plus, minus }) => {
-  return (
-    <div className="App">
-      <h1>Hello Redux</h1>
-      <h2>{number}</h2>
-      <button
-        onClick={() => {
-          plus(10)
-        }}
-      >
-        CountUp
-      </button>
-      <button
-        onClick={() => {
-          minus(10)
-        }}
-      >
-        CountDown
-      </button>
-    </div>
-  )
+    return (
+        <div className='App'>
+            <h1>Hello Redux</h1>
+            <h2>{number}</h2>
+            <button
+                onClick={() => {
+                    plus(10)
+                }}
+            >
+                CountUp
+            </button>
+            <button
+                onClick={() => {
+                    minus(10)
+                }}
+            >
+                CountDown
+            </button>
+        </div>
+    )
 }
 
 const mapStateProps = (state) => {
-  return {
-    number: state,
-  }
+    return {
+        number: state,
+    }
 }
 const mapDispatchToProps = (dispatch) => {
-  return {
-    plus: (num) => {
-      dispatch({ type: 'PLUS', payload: { num: num } })
-    },
-    minus: (num) => {
-      dispatch({ type: 'MINUS', payload: { num: num } })
-    },
-  }
+    return {
+        plus: (num) => {
+            dispatch({ type: 'PLUS', payload: { num: num } })
+        },
+        minus: (num) => {
+            dispatch({ type: 'MINUS', payload: { num: num } })
+        },
+    }
 }
 
 export default connect(mapStateProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -5,17 +5,21 @@ import { createStore } from 'redux'
 import { reducer } from './reducer'
 import App from './App'
 
-const store = createStore(reducer)
+const store = createStore(
+    reducer /* preloadedState, */,
+    // for Extention Redux Debug
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 store.subscribe(() => {
-  console.log(store.getState())
+    console.log(store.getState())
 })
 // store.dispatch({ type: "MINUS", payload: { num: 10 } });
 
 const rootElement = document.getElementById('root')
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  rootElement,
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
 )
